ad0bd644b546e533c74f9fb1fe2d7feb9e9d47ab
geforcexfx <geforcexfx@yahoo.com>
2018-02-08 09:47:57 +0100

    register user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,68 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+var upload = multer({dest: './uploads'});
 
+var User = require('../models/user');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/register', function(req, res, next) {
+  res.render('register',{title:'Register'});
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login',{title:'Login'});
+});
+router.post('/register', upload.single('profileimage'),function(req, res, next) {
+  var name = req.body.name;
+  var email = req.body.email;
+  var username = req.body.username;
+  var password = req.body.password;
+  var password2 = req.body.password2;
+
+  if(req.file){
+    console.log('Upload...');
+    var profileimage = req.file.filename;
+  }
+  else{
+    console.log('no file...');
+    var profileimage = "noimage.jpg";
+  }
+  // form Validator
+  req.checkBody('name', 'Name field is required').notEmpty();
+  req.checkBody('email', 'Email field is required').notEmpty();
+  req.checkBody('email', 'Email is not valid').isEmail();
+  req.checkBody('username', 'UserName field is required').notEmpty();
+  req.checkBody('password', 'Password field is required').notEmpty();
+  req.checkBody('password2', 'Password do not match').equals(req.body.password);
+  //Check errors
+  var errors = req.validationErrors();
+
+  if(errors){
+    res.render('register',{
+      errors: errors
+    });
+    console.log('Errors');
+  }
+  else{
+    var newUser = new User({
+      name: name,
+      email: email,
+      username: username,
+      password: password,
+      profileimage: profileimage
+    });
+
+    User.createUser(newUser, function(err, user){
+      if(err) throw err;
+      console.log(user);
+    });
+    res.location('/');
+    res.redirect('/');
+    console.log('no error');
+  }
+});
 module.exports = router;
